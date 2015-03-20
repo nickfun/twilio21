31fca24f4e5fa02917054d5f02438372d3006469
Nicholas Funnell <nick@nick.gs>
2015-03-20 10:08:29 -0700

    dropt the Session class, just use a dict. add a /json endpoint to introspect the sessions

--- blackjack.py
+++ blackjack.py
@@ -1,32 +1,32 @@
+#
+# Twilio Blackjack
+#
+# Nicholas Funnell
+# nick@nick.gs
+# 2015-03-19 
+#
 
-#
-# Twilio Blackjac
-#
-# Nicholas Funnell 2015-03-19 nick@nick.gs
-#
-
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 from random import randint
 import pprint
+import json
 
 server = Flask("blackjack")
 pp = pprint.PrettyPrinter(indent=4)
 db = {}
 host = "http://deba25b.ngrok.com"
 
-class SessionStruct(object):
-    def __init__(self, count=None, wins=None, playerHand=None, dealerHand=None):
-        self.count = count
-        self.wins = wins
-        self.playerHand = playerHand
-        self.dealerHand = dealerHand
-
 def getSession(number):
     if db.has_key(number):
         print "Session Exists! {}".format(number)
         return db[number]
     else:
-        db[number] = SessionStruct()
+        db[number] = {
+            "count": 0,
+            "wins": 0,
+            "playerHand": 0,
+            "dealerHand": 0
+        }
         print "Session CREATED {}".format(number)
         return db[number]
 
@@ -43,16 +43,18 @@ def main():
     tplData['host'] = host
     if request.values.get('CallStatus') == "ringing":
         # the first call
-        session.count = 0
-        session.wins = 0
-        session.playerHand = calculateHand()
-        session.dealerHand = calculateHand()
+        session['count'] = 0
+        session['wins'] = 0
+        session['playerHand'] = calculateHand()
+        session['dealerHand'] = calculateHand()
         tplData['first_message'] = "Welcome to Twilio 21 the app by Nick your dealer is Jason"
-    session.count = session.count + 1
-    tplData['gather_message'] = 'this is the gather message I hope you enjoy it a lot. you are in session {}'.format(session.count)
+    session['count'] += 1
+    tplData['gather_message'] = 'this is the gather message I hope you enjoy it a lot. you are in session {}'.format(session['count'])
+    session['playerHand'] = calculateHand(session['playerHand'])
+    session['dealerHand'] = calculateHand(session['dealerHand'])
     # save session
     db[request.values.get('From')] = session
-    return render_template("twiml.xml", t=tplData)
+    return render_template("twiml.xml", t=tplData, s=session)
 
 @server.route("/sessions")
 def showSessions():
@@ -63,7 +65,12 @@ def showSessions():
     for key in db:
         result = result + "- {}\n".format(key)
     return result
+
+@server.route("/json")
+def showAllSession():
+    resp = Response(json.dumps(db, indent=4))
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
     
 # RUN SERVER
 server.run(debug=True)
-print "server is running :-)"
